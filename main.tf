0106209b7b73251580050b93d584346066f46b8b
Kunlesanni <42106750+Kunlesanni@users.noreply.github.com>
2023-05-14 23:14:08 +0100

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,28 @@
+terraform {
+  cloud {
+    organization = "Five9hot"
+
+    workspaces {
+      name = "gha_dev"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "testgha" {
-  name     = var.name
-  location = location
+      name     = var.rg
+    location = location
   tags     = {
     Environment = var.environment
   }
+}
+
+
+variable "rg" {
+  type = string
+  description = "name of the resource group"
+}
+
+variable "environment" {
+  type = string
+  description = "name of the environment"
 }
