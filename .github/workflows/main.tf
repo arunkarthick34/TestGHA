1da3af8feb76f899285994ea1ee5b64e76745741
osanni <42106750+Kunlesanni@users.noreply.github.com>
2023-05-14 07:47:17 +0100

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_resource_group" "testgha" {
   name     = var.name
   location = location
   tags     = {
-    Environment = var.enviroment
+    Environment = var.environment
   }
 }
