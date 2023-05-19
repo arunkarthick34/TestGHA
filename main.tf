d502f4425d757d99ccb316d7551444f86a821078
Olakunle Sanni <ccaeosa@ucl.ac.uk>
2023-05-19 13:12:05 +0100

    update

--- main.tf
+++ main.tf
@@ -1,21 +1,47 @@
-terraform {
-  cloud {
-    organization = "Five9hot"
+# terraform {
+#   cloud {
+#     organization = "university-college-london"
 
-    workspaces {
-      name = "gha_dev"
+#     workspaces {
+#       name = "TestGHA"
+#     }
+#   }
+# }
+
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version =   "3.56.0"
+#     }
+#   }
+# }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.56.0"
     }
   }
+  cloud {}
 }
 
+# provider "azurerm" {
+#   features {}
+#   skip_provider_registration = true
+# }
+
+
 provider "azurerm" {
-  features {}
+        use_cli = true
+    features {}
 }
 
 resource "azurerm_resource_group" "testgha" {
       name     = var.rg
-    location = "uk south"
-  tags     = {
+  location = "uk south"
+  tags = {
     Environment = var.environment
   }
 }
